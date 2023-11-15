f8a91fd69f01d2457f936bd49a67288ae566e579
“Jevin” <jevin@mogul-tech.com>
2023-11-15 18:07:59 +0800

    fix: update

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
-import Components from 'unplugin-vue-components/vite'
+// import Components from 'unplugin-vue-components/vite'
 
 import { MarkdownTransform } from './.vitepress/plugins/markdown-transform'
 
